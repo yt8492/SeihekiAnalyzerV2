6194b8e78f70594d60236e1ba981aa79c8f92837
yt8492 <tmymyut2000@gmail.com>
2019-12-15 00:41:08 +0900

    add server

--- server/src/main/kotlin/com/yt8492/seihekianalyzerv2/server/ServerMain.kt
+++ server/src/main/kotlin/com/yt8492/seihekianalyzerv2/server/ServerMain.kt
@@ -1,5 +1,16 @@
 package com.yt8492.seihekianalyzerv2.server
 
-fun main() {
+import io.grpc.ServerBuilder
 
+fun main() {
+    println("start server")
+    val port = 50051
+    val server = ServerBuilder.forPort(port)
+        .build()
+        .start()
+    Runtime.getRuntime().addShutdownHook(Thread {
+        server.shutdown()
+        println("server shutdown")
+    })
+    server.awaitTermination()
 }

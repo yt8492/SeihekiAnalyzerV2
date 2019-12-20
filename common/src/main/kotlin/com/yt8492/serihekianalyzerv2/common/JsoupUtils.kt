753ceb24db0010d986212a5f039c3c505399a470
yt8492 <tmymyut2000@gmail.com>
2019-12-20 18:13:24 +0900

    fix JsoupUtil

--- common/src/main/kotlin/com/yt8492/serihekianalyzerv2/common/JsoupUtils.kt
+++ common/src/main/kotlin/com/yt8492/serihekianalyzerv2/common/JsoupUtils.kt
@@ -2,9 +2,10 @@ package com.yt8492.serihekianalyzerv2.common
 
 import org.jsoup.Connection
 import org.jsoup.Jsoup
+import java.net.URL
 
 object JsoupUtils {
-    fun requestByGet(url: String, cookies: Map<String, String>? = null, data: Map<String, String>? = null): Connection.Response {
+    fun requestByGet(url: URL, cookies: Map<String, String>? = null, data: Map<String, String>? = null): Connection.Response {
         var connection =
             getConnection(url)
         if (cookies != null) {
@@ -16,7 +17,7 @@ object JsoupUtils {
         return connection.method(Connection.Method.GET).execute()
     }
 
-    fun requestByPost(url: String, cookies: Map<String, String>? = null, data: Map<String, String>? = null): Connection.Response {
+    fun requestByPost(url: URL, cookies: Map<String, String>? = null, data: Map<String, String>? = null): Connection.Response {
         var connection =
             getConnection(url)
         if (cookies != null) {
@@ -28,8 +29,8 @@ object JsoupUtils {
         return connection.method(Connection.Method.POST).execute()
     }
 
-    fun getConnection(url: String, sleepTimeMilliSec: Long = 0): Connection {
+    fun getConnection(url: URL, sleepTimeMilliSec: Long = 0): Connection {
         Thread.sleep(sleepTimeMilliSec)
-        return Jsoup.connect(url).timeout(100000).maxBodySize(0)
+        return Jsoup.connect(url.toString()).timeout(100000).maxBodySize(0)
     }
 }

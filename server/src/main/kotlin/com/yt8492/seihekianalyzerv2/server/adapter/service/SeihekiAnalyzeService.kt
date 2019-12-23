198b44f97a1dee52aaea1f307faba380652da0e1
yt8492 <tmymyut2000@gmail.com>
2019-12-23 11:32:41 +0900

    implement SeihekiAnalyzeService

---
+++ server/src/main/kotlin/com/yt8492/seihekianalyzerv2/server/adapter/service/SeihekiAnalyzeService.kt
@@ -0,0 +1,41 @@
+package com.yt8492.seihekianalyzerv2.server.adapter.service
+
+import com.yt8492.seihekianalyzerv2.proto.AnalyzeResult as AnalyzeResultProto
+import com.yt8492.seihekianalyzerv2.proto.SeihekiAnalyzerCoroutineGrpc
+import com.yt8492.seihekianalyzerv2.proto.Tag as TagProto
+import com.yt8492.seihekianalyzerv2.proto.TagCount as TagCountProto
+import com.yt8492.seihekianalyzerv2.server.usecase.analyze.SeihekiAnalyzeResult
+import com.yt8492.seihekianalyzerv2.server.usecase.analyze.SeihekiAnalyzeUseCase
+import com.yt8492.serihekianalyzerv2.common.domain.model.Url
+import com.yt8492.seihekianalyzerv2.proto.Urls as UrlsProto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+class SeihekiAnalyzeService(
+    private val seihekiAnalyzeUseCase: SeihekiAnalyzeUseCase
+) : SeihekiAnalyzerCoroutineGrpc.SeihekiAnalyzerImplBase() {
+    override val initialContext: CoroutineContext
+        get() = Dispatchers.Default + Job()
+
+    override suspend fun analyze(request: UrlsProto): AnalyzeResultProto {
+        val urls = request.urlsList.map { Url(it.value) }
+        return when (val result = seihekiAnalyzeUseCase.execute(urls)) {
+            is SeihekiAnalyzeResult.Success -> {
+                AnalyzeResultProto {
+                    totalCount = result.result.totalCount
+                    val tagCounts = result.result.tagCounts.map {
+                        TagCountProto {
+                            tag = TagProto {
+                                value = it.tag.value
+                            }
+                            count = it.count
+                        }
+                    }
+                    addAllTagCounts(tagCounts)
+                }
+            }
+            is SeihekiAnalyzeResult.Failure -> throw result.cause
+        }
+    }
+}
--- server/src/main/kotlin/com/yt8492/seihekianalyzerv2/server/domain/service/AnalyzeService.kt
+++
@@ -1,8 +0,0 @@
-package com.yt8492.seihekianalyzerv2.server.domain.service
-
-import com.yt8492.serihekianalyzerv2.common.domain.model.AnalyzeResult
-import com.yt8492.serihekianalyzerv2.common.domain.model.Url
-
-interface AnalyzeService {
-    suspend fun analyze(urls: List<Url>): AnalyzeResult
-}

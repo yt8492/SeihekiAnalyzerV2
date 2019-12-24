c571bdee591a2c0c47597af8f18f54ccdaeac02c
yt8492 <tmymyut2000@gmail.com>
2019-12-24 10:20:22 +0900

    add tweet

--- app/src/main/java/com/yt8492/seihekianalyzerv2/ui/analyze/AnalyzeResultFragment.kt
+++ app/src/main/java/com/yt8492/seihekianalyzerv2/ui/analyze/AnalyzeResultFragment.kt
@@ -1,6 +1,8 @@
 package com.yt8492.seihekianalyzerv2.ui.analyze
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,12 +11,13 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yt8492.seihekianalyzerv2.databinding.FragmentAnalyzeResultBinding
+import com.yt8492.seihekianalyzerv2.ui.bindingmodel.AnalyzeResultBindingModel
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
+
 
 class AnalyzeResultFragment : Fragment() {
 
@@ -44,6 +47,11 @@ class AnalyzeResultFragment : Fragment() {
             addItemDecoration(dividerItemDecoration)
             adapter = recyclerAdapter
         }
+        binding.tweetButton.setOnClickListener {
+            viewModel.analyzeResult.value?.let {
+                tweetSeiheki(it)
+            }
+        }
         viewModel.analyzeResult.observe(viewLifecycleOwner, Observer {
             val tagCounts = it.tagCounts
             recyclerAdapter.submitList(tagCounts)
@@ -52,6 +60,20 @@ class AnalyzeResultFragment : Fragment() {
             Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
         })
         return binding.root
+    }
+
+    private fun tweetSeiheki(analyzeResult: AnalyzeResultBindingModel) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        val content = buildString {
+            appendln("Seiheki Analyze Result")
+            appendln("購入数: ${analyzeResult.totalCount}")
+            analyzeResult.tagCounts.forEach {
+                appendln("${it.tag.value}: ${String.format("%.2f", it.percentage)}")
+            }
+        }
+        val encodedContent = Uri.encode(content)
+        intent.data = Uri.parse("twitter://post?message=$encodedContent")
+        startActivity(intent)
     }
 
     override fun onAttach(context: Context) {

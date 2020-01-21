6a32696c46f6577739037879b4c199c57afae25e
yt8492 <tmymyut2000@gmail.com>
2020-01-22 08:44:33 +0900

    fix line breaks

--- common/src/main/kotlin/com/yt8492/seihekianalyzerv2/common/usecase/analyze/SeihekiAnalyzeUseCase.kt
+++ common/src/main/kotlin/com/yt8492/seihekianalyzerv2/common/usecase/analyze/SeihekiAnalyzeUseCase.kt
@@ -3,5 +3,7 @@ package com.yt8492.seihekianalyzerv2.common.usecase.analyze
 import com.yt8492.seihekianalyzerv2.common.domain.model.WorkNameAndUrl
 
 interface SeihekiAnalyzeUseCase {
-    suspend fun execute(workNameAndUrls: List<WorkNameAndUrl>): SeihekiAnalyzeResult
+    suspend fun execute(
+        workNameAndUrls: List<WorkNameAndUrl>
+    ): SeihekiAnalyzeResult
 }

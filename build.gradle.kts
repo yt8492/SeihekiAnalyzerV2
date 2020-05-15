9c3557c59e3fbf82e915f523c6a3ce87f1a0a374
yt8492 <tmymyut2000@gmail.com>
2020-05-16 00:29:30 +0900

    update gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,9 @@ buildscript {
         maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.1")
+        classpath("com.android.tools.build:gradle:3.6.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Dependencies.Kotlin.version}")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.2")
         classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.10")
         classpath("com.squareup.sqldelight:gradle-plugin:1.2.1")
         // NOTE: Do not place your application dependencies here; they belong
